d1d6834874a6cd64817c271fe54e939c104c87d3
Aditya Chatterjee <aditianhacker@gmail.com>
2018-06-07 15:37:08 +0530

    initial app

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('h/index.html')  
+
+@app.route('/cosmos')
+def home_cosmos():
+    return render_template('h/cosmos.html')  
+
+@app.route('/quark')
+def home_quark():
+    return render_template('h/quark.html')  
+
+@app.route('/search')
+def home_search():
+    return render_template('h/search.html')  
+
+@app.route('/iq')
+def home_iq():
+    return render_template('h/iq.html') 
+
+@app.route('/discuss')
+def home_discuss():
+    return render_template('h/discuss.html')  
+
+@app.route('/logo')
+def logo():
+	return send_from_directory('static/files', 'logo.jpg')
+
+if __name__ == '__main__':
+	app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
