eb630ba8abfb46af6dc8606b973d9f79c7b667e6
Aditya Chatterjee <virtualadichat@gmail.com>
2018-06-14 19:21:16 +0530

    fixed debug to false

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def logo():
 	return send_from_directory('static/files', 'logo.jpg')
 
 if __name__ == '__main__':
-	app.run(debug=True,host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+	app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
